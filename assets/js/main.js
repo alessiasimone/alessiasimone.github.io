d10c195ce855a433efe3ddca4535437a66224406
Alessia Simone <simone.alessia91@gmail.com>
2023-10-01 00:21:41 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,7 +34,7 @@ const skillsContent = document.getElementsByClassName('skills__content'),
 skillsHeader = document.querySelectorAll('.skills__header')
 
 function toggleSkills(){
-    let itemClass = this.parentNode.getElementsByClassName
+    let itemClass = this.parentNode.className
 
     for(i = 0; i < skillsContent.length; i++){
         skillsContent[i].className = 'skills__content skills__close'
@@ -43,6 +43,10 @@ function toggleSkills(){
         this.parentNode.className = 'skills__content skills__open'
     }
 }
+
+skillsHeader.forEach((el) =>{
+    el.addEventListener('click', toggleSkills)
+})
 
 /*==================== QUALIFICATION TABS ====================*/
 
